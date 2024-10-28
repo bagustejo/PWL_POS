3da2a9d3430db3ee55c784ed35bc12ddcf8fda85
bagustejo <bagustejomlg@gmail.com>
2024-10-28 10:03:49 +0700

    2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,20 @@ class UserController extends Controller
         return view('user', ['data' => $user]);
         */
 
-        $jumlahUser = UserModel::where('level_id', 2)->count();
-        return view('user', ['jumlahUser'=> $jumlahUser]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager 11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+        $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
